7cd63d39a2f3e116fa080ac27fa24183e3971254
Mauí Buarque <maui.buarque1@gmail.com>
2024-02-07 15:03:46 -0300

    feat: components & globalStyle

--- burger-code/src/App.jsx
+++ burger-code/src/App.jsx
@@ -1,5 +1,17 @@
+import "./styles/index.scss";
+
+import Header from "./components/Header";
+import Dashboard from "./components/Dashboard";
+import Cart from "./components/Cart";
+
 const App = () => {
-  return <></>;
+  return (
+    <>
+      <Header />
+      <Dashboard />
+      <Cart />
+    </>
+  );
 };
 
 export default App;
