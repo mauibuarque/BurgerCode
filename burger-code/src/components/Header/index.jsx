4e13f7e9af7e26ee2b2b7cd181e77ec3a526d7e8
Mauí Buarque <maui.buarque1@gmail.com>
2024-02-07 15:45:32 -0300

    feat: header

--- burger-code/src/components/Header/index.jsx
+++ burger-code/src/components/Header/index.jsx
@@ -1,5 +1,23 @@
+import style from "./style.module.scss";
+
+import cartImg from "../../assets/cart.svg";
+
 const Header = () => {
-  return <></>;
+  return (
+    <>
+      <div className="container">
+        <header className={style.header}>
+          <h1 className="fontPrimary bold">
+            Burguer<span>Code</span>
+          </h1>
+          <div>
+            <img src={cartImg} alt="Carrinho" />
+            <span>0</span>
+          </div>
+        </header>
+      </div>
+    </>
+  );
 };
 
 export default Header;
