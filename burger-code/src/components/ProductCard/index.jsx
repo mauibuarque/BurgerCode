75f11cce9947a58869ce7d3b7d57229a6308ec89
Mauí Buarque <maui.buarque1@gmail.com>
2024-02-08 17:35:29 -0300

    fix: productCardStyleClasses

--- burger-code/src/components/ProductCard/index.jsx
+++ burger-code/src/components/ProductCard/index.jsx
@@ -13,14 +13,14 @@ const ProductCard = ({ product }) => {
     <>
       <li key={id} className={style.card}>
         <img src={img} alt={`Foto ${name}`} />
-        <section className="fontPrimary">
-          <div>
+        <div className={style.cardContent}>
+          <div className={style.cardTitlePrice}>
             <h1 className="bold">{name}</h1>
             <span className="semibold">{formatPrice(price)}</span>
           </div>
-          <p className="regular">{category}</p>
+          <p className={`${style.category} regular`}>{category}</p>
           <button className="button semibold">Adicionar</button>
-        </section>
+        </div>
       </li>
     </>
   );
